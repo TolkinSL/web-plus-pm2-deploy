3e3ac5d1cb3da7d0ee4587d266269abeb2d02f73
TolkinSL <tolkinsl@yandex.ru>
2024-06-02 16:21:33 +0300

    DP06 Fix Frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.tolkinsl.nomorepartiesco.ru/');
+  const api = new Api('https://api.tolkinsl.nomorepartiesco.ru');
 
   export default api;
